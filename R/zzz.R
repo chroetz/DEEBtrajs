0cc8b623bf5c7b0d9e87f4a85d23cf52a96b3b9b
Christof Schoetz <cschoetz@pik-potsdam.de>
2022-09-21 16:07:18 +0200

    fix

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   v <- getOption("DEETrajs.validate")
-  if (!is.null(v)) {
+  if (is.null(v)) {
     options(DEETrajs.validate = TRUE)
   }
   invisible(NULL)
