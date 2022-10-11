360ae406513f57576e7d9c76134fdbfd0345b67b
Christof Schoetz <cschoetz@pik-potsdam.de>
2022-10-11 15:25:54 +0200

    documentation for projection

--- R/projection.R
+++ R/projection.R
@@ -1,3 +1,7 @@
+#' Return the identity projection.
+#'
+#' @return A named list of two function: `project(x)` and `embed(x)`, both
+#'   realize the identity function.
 #' @export
 getIdentityProjection <- function() {
   list(
@@ -5,6 +9,18 @@ getIdentityProjection <- function() {
     embed = \(x) x)
 }
 
+#' Calculate linear projection from data
+#'
+#' Calculates a PCA based linear projection to a `dim` space from a
+#' `d = ncol(y)` dimensional space from `n = nrow(y)` data point stored in `y`.
+#'
+#' @param y A numeric matrix.
+#' @param dim A single positive integer. The target dimension.
+#' @return A named list of two function: `project(x)` maps `d` dimensional data
+#'   stored in a matrix `x` with `d` columns to the lower dimensional space and
+#'   outputs the respective values as matrix with the same number of rows as the
+#'   input and `dim` columns. `embed(x)` maps `dim` dimensional data back to the
+#'   `d` dimensional space, again via matrices with the respective column count.
 #' @export
 calculateProjection <- function(y, dim) {
   d <- ncol(y)
