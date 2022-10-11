f1d2f0b127ce165a33ba1b0c0d7392078a742ff6
Christof Schoetz <cschoetz@pik-potsdam.de>
2022-10-11 21:38:09 +0200

    make bindTrajs return NULL on empty list

--- R/bind.R
+++ R/bind.R
@@ -1,5 +1,7 @@
 #' @export
 bindTrajs <- function(...) {
+  if (...length() == 0) return(NULL)
+  if (...length() == 1 && length(..1) == 0) return(NULL)
   res <- dplyr::bind_rows(...)
   return(asTrajs(res))
 }
